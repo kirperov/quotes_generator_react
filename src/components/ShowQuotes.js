8547d26d303ae0c724d3e988f5c8b58e696eabdf
kirperov <kirill.petrov.centrale@gmail.com>
2020-11-02 18:17:14 +0100

    optimisation function generateQuote + optimisation ShowQuotes

--- src/components/ShowQuotes.js
+++ src/components/ShowQuotes.js
@@ -3,21 +3,19 @@ import React from 'react';
 const ShowQuotes = quotes => {
   let html=null;
   const listQuotes = quotes.quotes;
-  const renderListQuotes = listQuotes.map((quote) =>
-    <li key={quote}>{quote.join(' ')}</li>
-  );
   if(listQuotes.length === 0) {
       html =  
-        <div  className="mt-3 col-md-6">
-            <div className="alert alert-primary" role="alert">
-                Veillez choisir le thème et nombre de citations
-          </div>
+        <div className="mt-3 col-md-6">
+            <p className="alert alert-primary" role="alert"> Veillez choisir le thème et nombre de citations </p>
         </div>;
   } else {
+    const renderListQuotes = listQuotes.map((quote) =>
+    <li key={quote}>{quote.join(' ')}</li>
+    );
     html = 
-    <div id="quotes" className="bg-light mt-3 col-md-6">
-      <ul>{renderListQuotes}</ul>
-    </div>;
+      <div id="quotes" className="bg-light mt-3 col-md-6">
+        <ul>{renderListQuotes}</ul>
+      </div>;
   }
 
   return (
--- src/components/quoteGenerator.js
+++ src/components/quoteGenerator.js
@@ -3,25 +3,30 @@ import { dataQuotes} from './constants.js';
 export function main(selectTheme,selectNumbers) {  
     selectTheme = parseInt(selectTheme);
     selectNumbers = parseInt(selectNumbers);
-    let generatedQuote = [];
-    let numberRand;
-    let quotes =dataQuotes();
+    let generatedQuote = [],
+        numberRand,
+        quotes = dataQuotes(),
+        noRepeatQuote;
 
     function generateRandomNumber(num) {
         return Math.floor((Math.random() * num));
     }
  
-    if(selectTheme <0 | selectNumbers <0 | selectTheme > 1 | selectNumbers > 5) {
-        return generatedQuote;
-    } else {
+    function generateQuote() {
         for(let i = 0; i < selectNumbers; i++) {     
             generatedQuote[i] = [];
             for(let n = 0; n <quotes[selectTheme].length; n++) {
                 numberRand = generateRandomNumber(quotes[selectTheme][n].length);
-                let noRepeatQuote = quotes[selectTheme][n].splice(numberRand,1);  
+                noRepeatQuote = quotes[selectTheme][n].splice(numberRand,1);  
                 generatedQuote[i].push(noRepeatQuote);
-            }               
+            }            
         }
         return generatedQuote;
+    }
+    
+    if(selectTheme <0 | selectNumbers <0 | selectTheme > 1 | selectNumbers > 5) {
+        return generatedQuote;
+    } else {
+        return generateQuote();
     }
 }
